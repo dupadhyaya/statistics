68815fc8af895bb33670c4e3bbf1e1d196ab1ea9
Dhiraj Upadhyaya <dup1966@gmail.com>
2016-11-30 15:20:51 +0530

    n

---
+++ htestchisq2.R
@@ -0,0 +1,13 @@
+# Chi squared Tests 2
+# http://stattrek.com/chi-square-test/independence.aspx?Tutorial=AP
+
+# Q1 ----------------------------------------------------------------------
+
+#Ho: Gender and voting preferences are independent. 
+#Ha: Gender and voting preferences are not independent.
+
+#Formulate an analysis plan. For this analysis, the significance level is 0.05.
+
+#Analyze sample data. Applying the chi-square test for independence to sample data, 
+#we compute the degrees of freedom, the expected frequency counts, and the chi-square test statistic. 
+#Based on the chi-square statistic and the degrees of freedom, we determine the P-value.

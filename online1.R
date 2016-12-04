d0eb83bc251c92b242387f2f7f458976d4deef4a
dupadhyaya <dup1966@gmail.com>
2016-12-04 17:35:59 +0530

    d

--- online1.R
+++ online1.R
@@ -1,1 +1,28 @@
 #Online Class in R : 03 Dec 16
+# average is the best model
+#advertisement vs Sales 
+#line passing through closest to each point
+
+library(boot)
+libary(car)
+#libary(Quant)
+library(lmtest)
+library(sandwich)
+library(vars)
+library(nortest)
+library(MASS)
+
+
+
+
+
+# Model -------------------------------------------------------------------
+# Stepwise Regression ?
+
+nrow(data)
+names(data)
+# DV - sales , IVs ---
+fit = lm(sales ~ Item_Weight + Item_Fact_Content, Outler_Identifie, data = data)
+summary(fit)
+
+
